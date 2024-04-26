6cde0253cd493efbd98ddcd75b3e46377601d01c
aliyaseitova <230587@astanait.edu.kz>
2024-04-26 07:13:49 +0500

    Commit

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -70,7 +70,7 @@ public class MyLinkedList<E> implements MyList<E> {
 
     public E get(int index) {
         if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException("The index is out of bound");
+            throw new IndexOutOfBoundsException("The index is out of bounds");
         }
         return getNode(index).data;
     }
